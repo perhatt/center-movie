fe9dd7a95b00055a0fc7954c7d1b8e938de2e252
mahui <123@qq.com>
2024-03-20 18:08:13 +0800

    list components done

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,35 +1,38 @@
+<script setup>
+const props = defineProps({
+  list: String,
+});
+</script>
+
 <template>
-  <div class="w-56 h-72 rounded-xl relative overflow-hidden group">
-    <img
-      src="https://picsum.photos/196/252"
-      class="w-full h-full rounded-xl"
-      alt=""
-    />
+  <div
+    class="w-56 h-72 rounded-xl relative overflow-hidden group cursor-pointer"
+  >
+    <img :src="props.list.cover" class="w-full h-full rounded-xl" alt="" />
     <div
-      class="absolute top-72 lef-0 w-full h-40 bg-black/50 backdrop-blur-sm z-50 group-hover:top-32 duration-100 ease-out text-white flex flex-col px-2"
+      class="absolute top-64 lef-0 w-full h-40 bg-black/50 backdrop-blur-sm group-hover:top-32 transition-all duration-100 ease-out text-white flex flex-col px-2"
     >
-      <p
-        class="w-full truncate flex-wrap text-md leading-8 hover:underline cursor-pointer"
-      >
-        TItle namewfweffwfwefe fwefewf
+      <p class="w-full truncate flex-wrap text-md leading-8 hover:underline">
+        {{ props.list.title }}
       </p>
       <div class="w-full h-10 flex items-center gap-1">
         <div
-          v-for="i in 3"
+          v-for="i in props.list.tab"
           :key="i"
           class="w-fit px-2 h-6 bg-white/10 rounded-xl text-xs flex items-center justify-center"
         >
-          121221
+          {{ i }}
         </div>
       </div>
       <p class="w-full line-clamp-4 overflow-hidden text-xs">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. dolorum?
+        {{ props.list.introduce }}
       </p>
     </div>
     <div
+      v-if="props.list.lebal.length"
       class="absolute top-0 right-0 w-fit bg-red-500 px-2 py-1 text-xs text-white rounded-bl"
     >
-      高清电影
+      {{ props.list.lebal }}
     </div>
   </div>
 </template>
--- src/views/home/Index.vue
+++ src/views/home/Index.vue
@@ -1,23 +1,61 @@
 <script setup>
-  import Banner from "@/components/Banner.vue";
-  import List from "@/components/List.vue";
-  import { ref } from "vue";
-  const imgList = ref([
-    {
-      imgUrl: "https://picsum.photos/1040/300?random=1",
-      lebal: "wosivivivbisdv",
-    },
-    {
-      imgUrl: "https://picsum.photos/1040/300",
-      lebal: "qbufiwqfiwef",
-    },
-    {
-      imgUrl: "https://picsum.photos/1040/300?random=4",
-      lebal: "123456787654345y",
-    },
-  ]);
+import Banner from "@/components/Banner.vue";
+import List from "@/components/List.vue";
+import { ref } from "vue";
+const imgList = ref([
+  {
+    imgUrl: "https://picsum.photos/1040/300?random=1",
+    lebal: "wosivivivbisdv",
+  },
+  {
+    imgUrl: "https://picsum.photos/1040/300",
+    lebal: "qbufiwqfiwef",
+  },
+  {
+    imgUrl: "https://picsum.photos/1040/300?random=4",
+    lebal: "123456787654345y",
+  },
+]);
+const list = ref([
+  {
+    title: "模拟标题xxxxx",
+    tab: ["1121", "2121212", "12122121"],
+    cover: "https://picsum.photos/224/288",
+    introduce:
+      "ofuwefewfwefwefwefwefwefwefweffweffwecfwe er ergregergergregergerwgre",
+    lebal: "高清电影",
+  },
+  {
+    title: "模拟标题xxxxx",
+    tab: ["1121", "2121212", "12122121"],
+    cover: "https://picsum.photos/224/288",
+    introduce:
+      "ofuwefewfwefwefwefwefwefwefweffweffwecfwe er ergregergergregergerwgre",
+    lebal: "高清电影",
+  },
+  {
+    title: "模拟标题xxxxx",
+    tab: ["1121", "2121212", "12122121"],
+    cover: "https://picsum.photos/224/288",
+    introduce:
+      "ofuwefewfwefwefwefwefwefwefweffweffwecfwe er ergregergergregergerwgre",
+    lebal: "高清电影",
+  },
+  {
+    title: "模拟标题xxxxx",
+    tab: ["1121", "2121212", "12122121"],
+    cover: "https://picsum.photos/224/288",
+    introduce:
+      "ofuwefewfwefwefwefwefwefwefweffweffwecfwe er ergregergergregergerwgre",
+    lebal: "",
+  },
+]);
 </script>
 <template>
   <Banner :imgList="imgList"></Banner>
-  <list />
+  <div class="flex gap-2 w-full flex-wrap justify-around p-10">
+    <div v-for="i in list" :key="i">
+      <list :list="i" />
+    </div>
+  </div>
 </template>
