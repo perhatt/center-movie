1dd2679318c36ac4a112affb88fe8c8238d7b48d
mahui <123@qq.com>
2024-04-03 18:07:32 +0800

    navbar

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,17 @@
   import Navbar from "./components/Navbar.vue";
   import Menu from "./components/Menu.vue";
   import Dialog from "./components/Dialog.vue";
+  import { ref } from "vue";  
+  const scrollPosition = ref(0);  
+    const maxScrollOpacity = 1; 
+    const maxScroll = 500; 
+  // 滚动处理函数  
+  const handleScroll = (e) => {  
+    scrollPosition.value = e.srcElement.scrollTop
+    const opacity = Math.min(scrollPosition.value / maxScroll, maxScrollOpacity);
+    console.log(opacity);
+    return `rgba(255,0,0,${opacity})`;
+  };  
 </script>
 
 <template>
@@ -10,8 +21,8 @@
   >
     <Menu></Menu>
     <div class="flex-1 h-full flex flex-col relative">
-      <Navbar></Navbar>
-      <div class="w-full h-full overflow-y-scroll relative">
+      <Navbar :class="handleScroll"></Navbar>
+      <div class="w-full h-full overflow-y-scroll relative" @scroll="handleScroll($event)">
         <router-view></router-view>
         <Dialog></Dialog>
       </div>
@@ -19,4 +30,30 @@
   </div>
 </template>
 
-<style scoped></style>
+<style  lang="scss">
+.ripple-button {  
+  position: relative;  
+  //display: inline-block;   
+  overflow: hidden;
+  
+  &::before {  
+    content: "";  
+    position: absolute;  
+    top: 50%;  
+    left: 50%;  
+    width: 0;  
+    height: 0;  
+    border-radius: 50%;  
+    background: rgba(#fdaa7d, 0.9);  
+    opacity: 0;  
+    transform: translate(-50%, -50%);  
+    transition: all 0.1s ease;  
+  }  
+  
+  &:active::before {  
+    width: 120%;  
+    height: 120%;  
+    opacity: 1;  
+  }  
+}
+</style>
